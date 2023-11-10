36cab4ab9a36ec24752e9828a0832a2a1cc93c22
Binayak0407 <150440695+Binayak0407@users.noreply.github.com>
2023-11-10 20:03:40 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 144553
+let accontEmail = "binayak04@gmail.com"
+var accountPassword = "12345"
+accountCity = "bbsr"
+
+// accountId = 2 // not allowed
+
+accontEmail="hc@hc.com"
+accountPassword = "212121"
+accountCity = "ctc"
+console.log(accountId);
+console.table([accountId,accontEmail,accountPassword,accountCity])
