b68c15647eec9e01e8475aeaf4d10e7e5fe49a4d
Binayak0407 <150440695+Binayak0407@users.noreply.github.com>
2023-11-10 20:42:53 +0000

    container config done

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -2,6 +2,7 @@ const accountId = 144553
 let accontEmail = "binayak04@gmail.com"
 var accountPassword = "12345"
 accountCity = "bbsr"
+let accountState;
 
 // accountId = 2 // not allowed
 
@@ -9,4 +10,13 @@ accontEmail="hc@hc.com"
 accountPassword = "212121"
 accountCity = "ctc"
 console.log(accountId);
-console.table([accountId,accontEmail,accountPassword,accountCity])
+
+/*
+prefer not to use var 
+because of issue in block scope and functional scope 
+
+*/
+
+
+
+console.table( [accountId,accontEmail,accountPassword,accountCity,accountState] );
--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -1,1 +1,30 @@
 "use strict";//treat all js code as newer version
+
+//alert(3+3) //we are using nodejs ,not browser
+
+console.log(3+
+    3); //code readability should be high
+
+console.log("binayak")
+
+
+
+let name ="Binayak"
+let age = 18
+let isloggedIn = false
+let state ;
+
+// premitive dataTypes
+// number =>2 to power 53
+// bigint
+// string => ""
+// boolean => true/fasle
+// null => standalone value
+// undefined => 
+// symbol => unique 
+
+
+//object
+
+console.log(typeof"Binayak")
+console.log(typeof "age")
